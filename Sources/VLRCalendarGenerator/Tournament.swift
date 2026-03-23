25d4bde7f8dd18e131143dfecefc48bcf0b6ee6c
Chinh Ngo <nucauthu@gmail.com>
2026-03-23 09:32:15 +0100

    add tournaments

--- Sources/VLRCalendarGenerator/Tournament.swift
+++ Sources/VLRCalendarGenerator/Tournament.swift
@@ -7,22 +7,41 @@
 
 // Ongoing and upcoming tournaments to be exact
 enum Tournament: String {
+    case vct2026AmericasStage1 = "VCT 2026: Americas Stage 1"
+    case vct2026EmeaStage1 = "VCT 2026: EMEA Stage 1"
+    case vct2026PacificStage1 = "VCT 2026: Pacific Stage 1"
+    case vct2026ChinaStage1 = "VCT 2026: China Stage 1"
     // swiftlint:disable:next inclusive_language
-    case vct2026MastersSantiago2026 = "Valorant Masters Santiago 2026"
+    case vct2026MastersLondon2026 = "Valorant Masters London 2026"
+    case vct2026Champions2026  = "Valorant Champions 2026"
 }
 
 let vctTournaments: [Tournament] = [
-    .vct2026MastersSantiago2026
+    .vct2026AmericasStage1,
+    .vct2026EmeaStage1,
+    .vct2026PacificStage1,
+    .vct2026ChinaStage1,
+    .vct2026MastersLondon2026,
+    .vct2026Champions2026
 ]
 
 let globalTournaments: [Tournament] = [
-    .vct2026MastersSantiago2026
+    .vct2026MastersLondon2026,
+    .vct2026Champions2026
 ]
 
-let emeaTournaments: [Tournament] = []
+let emeaTournaments: [Tournament] = [
+    .vct2026EmeaStage1
+]
 
-let chinaTournaments: [Tournament] = []
+let chinaTournaments: [Tournament] = [
+    .vct2026ChinaStage1
+]
 
-let pacificTournaments: [Tournament] = []
+let pacificTournaments: [Tournament] = [
+    .vct2026PacificStage1
+]
 
-let americasTournaments: [Tournament] = []
+let americasTournaments: [Tournament] = [
+    .vct2026AmericasStage1
+]
